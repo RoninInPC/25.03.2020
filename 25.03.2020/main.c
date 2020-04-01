8d5f6056dac438bfa2577e5935f45855bc9e043f
RoninInPC <62109908+RoninInPC@users.noreply.github.com>
2020-04-01 14:59:43 +0500

    Усложнил пароль

--- 25.03.2020/main.c
+++ 25.03.2020/main.c
@@ -12,15 +12,15 @@ int main() {
 	int exc;
 	scanf_s("%d", &exc);
 	if (exc == 1) {
-		printf("write password of 8 symbols: min 1 Big and 1 small letter\n");
+		printf("write password >= 20 symbols: Min 10 0-9 numbers and 2 special symbol\n");
 		int x;
 		int k = 0;
 		char* arr[1000];
 		while (1) {
 			x = getch();
-			if ((x >= 48 && x <= 57) || (x >= 65 && x <= 90) || (x >= 97 && x <= 122)) {
+			if ((x >= 48 && x <= 57) || (x >= 65 && x <= 90) || (x >= 97 && x <= 122) || (x >= 33 && x <= 38)) {
 				arr[k] = x;
-				k++;
+					k++;
 			}
 			if (x == 8) {
 				k--;
@@ -38,16 +38,18 @@ int main() {
 		int k2 = 0;
 		int k1 = 0;
 		for (int i = 0; i < k; i++) {
-			if (arr[i] >= 'A' && arr[i] <= 'Z') {
-				k2 = 1;
+			if (arr[i] >= 33 && arr[i] <= 38) {
+				k2++;
 			}
-			if (arr[i] >= 'a' && arr[i] <= 'z') {
-				k1 = 1;
+			if (arr[i] >= '0' && arr[i] <= '9') {
+				k1++;
 			}
 		}
-		if (k2 == 0 || k1 == 0 || k != 8) {
+		if (k2 < 2 || k1 < 10 || k < 20) {
 			printf("You have wrong password\n");
 		}
+		else
+			printf("You have hard password\n");
 	}
 	else
 	{
